e07a2a12762d85e7d1e311ce33eec9fae7fa0190
Teatov <kormlenie10@mail.ru>
2023-04-14 22:02:32 +1000

    Убран комментарий из хуков

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,6 @@
 import { auth } from '$lib/server/lucia';
 import type { Handle } from '@sveltejs/kit';
 
-
-// export const customHandle: Handle = async ({ event, resolve }) => {
-// 	// event.locals.auth = auth.handleRequest(event);
-// 	return await resolve(event);
-// };
-
-// export const handle: Handle = sequence(handleHooks(auth), customHandle);
-
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
 	return await resolve(event);
