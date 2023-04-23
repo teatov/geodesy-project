3b5a21ca0c2f9a7b446606249b9783f9dfb73345
Teatov <kormlenie10@mail.ru>
2023-04-23 15:28:52 +1000

    добавлен контейнер в layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,20 +6,21 @@
 	export let data: PageData;
 </script>
 
-<nav>
-	<ul>
-		<li><a href="/">На главную</a></li>
-		{#if !data.user}
-			<li>
-				<a href="/signup">Регистрация</a>
-			</li>
-			<li><a href="/login" role="button">Вход</a></li>
-		{:else}
-			<li>
-				<a href="/profile" role="button">Личный кабинет</a>
-			</li>
-		{/if}
-	</ul>
-</nav>
-
-<slot />
+<div class="container mx-auto mt-0 space-y-8 px-4 py-3 md:max-w-4xl">
+	<nav>
+		<ul>
+			<li><a href="/">На главную</a></li>
+			{#if !data.user}
+				<li>
+					<a href="/signup">Регистрация</a>
+				</li>
+				<li><a href="/login" role="button">Вход</a></li>
+			{:else}
+				<li>
+					<a href="/profile" role="button">Личный кабинет</a>
+				</li>
+			{/if}
+		</ul>
+	</nav>
+	<slot />
+</div>
