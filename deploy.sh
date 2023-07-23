3dfde40e0c591ee29cb0dfcfdfc32f005f0c8b8a
Teatov <hlebpoimenigleb@gmail.com>
2023-07-23 14:57:50 +1000

    поправлен деплой скрипт

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,10 @@ sudo -u www-data cp -r package.json package-lock.json ./build
 sudo -u www-data cp -r prisma/schema.prisma ./build/prisma
 
 cd ./build
-npm ci --omit dev
-npx prisma generate
+sudo -u www-data npm ci --omit dev
+sudo -u www-data npx prisma generate
 cd ../
 
-pm2 stop /build/index.js --name $app_name 2>/dev/null
-pm2 delete /build/index.js --name $app_name
-HOST=127.0.0.1 PORT=3000 pm2 start /build/index.js --name $app_name
+sudo -u www-data pm2 stop /build/index.js --name $app_name 2>/dev/null
+sudo -u www-data pm2 delete /build/index.js --name $app_name
+HOST=127.0.0.1 PORT=3000 sudo -u www-data pm2 start /build/index.js --name $app_name
